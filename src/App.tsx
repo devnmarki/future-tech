48fcc8098810bf9f2eb68584ebcc14b83751f667
Marko Smiljic <markosmiljic99@gmail.com>
2026-02-25 15:19:24 +0100

    chore: initialize react-router

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,25 @@
+import Home from "./pages/home";
+import News from "./pages/news";
+import { createBrowserRouter } from "react-router";
+import { RouterProvider } from "react-router/dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/news",
+    element: <News />,
+  },
+]);
+
 const App = () => {
-    return  <h1 className="text-3xl font-bold underline">Hello world!</h1>
-}
+  return (
+    <>
+      <RouterProvider router={router} />
+    </>
+  );
+};
 
 export default App;
